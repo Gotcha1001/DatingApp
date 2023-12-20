fefc2a7072267fcf0588686ab1976bdda4b30baf
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2023-12-20 14:34:17 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+app.UseMiddleware<ExeptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("https://localhost:4200"));
