c2a16f2222e619a9b1c02da79edaa7fad622ef09
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2023-12-22 17:28:21 +0200

    End of section 11

--- API/Dtos/UserDto.cs
+++ API/Dtos/UserDto.cs
@@ -6,5 +6,6 @@ namespace API.Dtos
     {
         public string UserName { get; set; }
         public string Token { get; set; }
+        public string PhotoUrl { get; set; }
     }
 }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -14,7 +14,7 @@ namespace API.Entities
         public string PublicId { get; set; }
 
         public int AppUserId { get; set; }
-        public AppUser appUser { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
