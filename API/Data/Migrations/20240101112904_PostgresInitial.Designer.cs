65e7993e53d016608a44e4004bd2d6011f596468
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2024-01-01 14:48:09 +0200

    End of section 21

--- API/Data/Migrations/20240101112904_PostgresInitial.Designer.cs
+++ API/Data/Migrations/20240101112904_PostgresInitial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231230174913_PostgresInitial")]
+    [Migration("20240101112904_PostgresInitial")]
     partial class PostgresInitial
     {
         /// <inheritdoc />
